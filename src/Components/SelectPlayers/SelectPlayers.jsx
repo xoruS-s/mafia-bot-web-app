cc7752ecf27e947bb06c7eb1ca61831ba758ec8d
xoruS-s <merzkiy.senpai@mail.ru>
2022-12-19 17:50:38 +0300

    -0.2

---
+++ src/Components/SelectPlayers/SelectPlayers.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './SelectPlayers.css';
+
+const SelectPlayers = () => {
+    const count_players = [];
+    for (let i = 5; i < 15; i++) {
+        count_players.push(i)
+    }
+
+    return (
+        <div className={'form'}>
+            <h1>Жители города</h1>
+            <select>
+                {count_players.map((count_player) => (
+                    <div>
+                        <option value={count_player}></option>
+                    </div>
+                ))}
+            </select>
+        </div>
+    );
+};
+
+export default SelectPlayers;

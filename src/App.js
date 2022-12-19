457ab389235321df8850394bb45fbc53e29cf8f7
xoruS-s <merzkiy.senpai@mail.ru>
2022-12-19 19:41:42 +0300

    -0.3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import './App.css';
-import { useEffect } from "react";
-import Button from "./Components/Button/Button";
 import SelectPlayers from "./Components/SelectPlayers/SelectPlayers";
 
-const tg = window.Telegram.WebApp;
-
 function App () {
-
-    useEffect(() => {
-        tg.ready();
-    }, []);
-
-    const onClose = () => {
-        tg.close()
-    }
     return (
         <div className="App">
-            <h1>Working</h1>
             <SelectPlayers />
         </div>
     );
--- src/Components/SelectPlayers/SelectPlayers.jsx
+++ src/Components/SelectPlayers/SelectPlayers.jsx
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './SelectPlayers.css';
+import Button from "../Button/Button";
 
+const tg = window.Telegram.WebApp;
+tg.MainButton.setParams({
+    text: 'Готово'
+});
+tg.MainButton.show();
+let test = [];
 const SelectPlayers = () => {
-    const count_players = [];
-    for (let i = 5; i < 15; i++) {
-        count_players.push(i)
+    useEffect(() => {
+        tg.ready();
+    }, []);
+
+    const onClose = () => {
+        tg.close()
     }
+    const count_players = ['', 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+    const [count, setCount] = useState('');
+
+    const count_inputs = (e) => {
+        setCount(e.target.value)
+        test = [];
+        for (let i = 1; i <= e.target.value; i++) {
+            test.push(i);
+        }
+    }
+    console.log(test)
+    let list = count_players.map((num, index) => (<option key={index}>{num}</option>));
+    let input_list = test.map((item) => (<input type="text" placeholder={item}/>));
+    console.log(input_list)
 
     return (
         <div className={'form'}>
             <h1>Жители города</h1>
-            <select>
-                {count_players.map((count_player) => (
-                    <div>
-                        <option value={count_player}>{count_player}</option>
-                    </div>
-                ))}
-            </select>
+            <div className={'select_form'}>
+                <div className={'cnt_players_div'}>
+                    <p className={'count_players'}>Выберите кол-во игроков</p>
+                    <select className={'select'} value={count} onChange={count_inputs}>{list}</select>
+                </div>
+                <div className={'input'}>{input_list}</div>
+            </div>
+            {/*<Button onClick={onClose}>Закрыть</Button>*/}
         </div>
     );
 };
